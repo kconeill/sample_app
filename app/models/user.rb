ecd3829db6c3c5631191a934ef3c3e300d651d83
Kasey O'Neill <kasey.e.oneill@gmail.com>
2020-01-24 20:16:26 +0000

    Create User method for remember token and digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  before_save { email.downcase! }
+  attr_accessor :remember_token
+  
+  before_save { self.email.downcase! }
   
   validates :name, presence: true, length: { maximum: 50 }
   
@@ -15,6 +17,17 @@ class User < ApplicationRecord
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+  
+  # Returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # Sets the remember_token and bypass validations (security measure)
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
   
   
