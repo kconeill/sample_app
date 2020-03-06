95407533beee655298642169c105fceb7bcef1ae
Kasey O'Neill <kasey.e.oneill@gmail.com>
2020-03-06 20:47:04 +0000

    Add friendly forwarding after login, show/index

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:kasey)
+  end
 
   test "layout links" do
     get root_path
@@ -20,6 +24,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+    
   end
 
 end
